c89d61590d1623d83586e668be8d546ce3dbcc23
MaximeJoanisse <maxjoanisse101@gmail.com>
2023-10-12 03:11:01 -0400

    Frontend prototype
    added basic components with navbar and router system for the react.

---
+++ UInnovateApp/src/pages/ObjectMenu.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "../components/NavBar";
+
+export function ObjectMenu(){
+    return(
+        <>
+        <NavBar/>
+        <div style={{textAlign:'center', fontSize:'60px', paddingTop:"40px"}}> Tables would go under here</div>
+        </>
+    );
+}
