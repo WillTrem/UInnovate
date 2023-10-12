aadd99929e8be33677079af234dec160ab3796ed
William Tremblay <wt22@videotron.ca>
2023-10-12 18:11:42 -0400

    Migrated dump data from public to application schema

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-pg_restore -U ${POSTGRES_USER} -d uinnovate_test_db /dumps/uinnovate-test-db-dump.backup -c --if-exists --no-owner --no-privileges 
+pg_restore -U ${POSTGRES_USER} -d uinnovate_test_db /dumps/uinnovate-test-db-dump.backup -n application -c --if-exists --no-owner --no-privileges 
--- init.sql
+++ init.sql
@@ -47,9 +47,8 @@ CREATE OR REPLACE VIEW meta.columns ("schema", "table", "column", "references_ta
 -- Creating role web_anon
 CREATE ROLE web_anon nologin;
 
-GRANT USAGE ON SCHEMA public TO web_anon;
+GRANT USAGE ON SCHEMA application TO web_anon;
 GRANT USAGE ON SCHEMA meta TO web_anon;
-GRANT SELECT ON public.customers TO web_anon;
 GRANT SELECT ON meta.schemas TO web_anon;
 GRANT SELECT ON meta.tables TO web_anon;
 GRANT SELECT ON meta.columns TO web_anon;
