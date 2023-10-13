d7f70a6ddbe6fc97d16419ec95aecfc9bb038a3a
MariaR001 <ma_iv@live.concordia.ca>
2023-10-13 17:23:50 -0400

    fixed some bugs related to e2e and component tests

--- UInnovateApp/cypress/e2e/api/api.cy.ts
+++ UInnovateApp/cypress/e2e/api/api.cy.ts
@@ -1,17 +1,16 @@
 /// <reference types="cypress" />
 // About API testing: https://docs.cypress.io/api/commands/request#Method-and-URL
-describe("Check https://api.publicapis.org/entries request", () => {
+describe("Check http://localhost:5173/ request", () => {
     it("Get 200 status", () => {
         cy.request({
             method: "GET",
-            url: `https://api.publicapis.org/entries`,
+            url: `http://localhost:5173/`,
         }).as("getEntries");
 
         // eslint-disable-next-line  @typescript-eslint/no-explicit-any
         cy.get("@getEntries").should((response: any) => {
             expect(response.status).to.eq(200);
             expect(response).to.have.property("headers");
-            expect(response.body).to.have.property("entries")
         });
     });
 });
