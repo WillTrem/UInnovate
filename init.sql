3b447e25d5ee65a4bf4c2c9d0130015e66160878
Samy Refik <samy.refik@gmail.com>
2023-10-13 13:02:25 -0400

    Added VMD export for table names as a string array

--- init.sql
+++ init.sql
@@ -7,6 +7,12 @@ create table test_schema.test_table (
   due timestamptz
 );
 
+create view alltables as 
+  select * 
+  from information_schema.tables
+  where table_schema not in ('pg_catalog', 'information_schema')
+  and table_schema not like 'pg_toast%';
+
 insert into test_schema.test_table (task) values
   ('this is test task 1'), ('this is test task 2');
 
@@ -15,4 +21,4 @@ create role web_anon nologin;
 grant usage on schema test_schema to web_anon;
 grant usage on schema public to web_anon;
 grant select on test_schema.test_table to web_anon;
-grant select on public.customers to web_anon;
+grant select on all tables in schema public to web_anon;
