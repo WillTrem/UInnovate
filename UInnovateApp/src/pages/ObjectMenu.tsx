4f348926207360b80092a722165e60e682814fbd
Samy Refik <samy.refik@gmail.com>
2023-10-13 14:12:14 -0400

    Added frontend changes to the ObjectMenu for future reference

--- UInnovateApp/src/pages/ObjectMenu.tsx
+++ UInnovateApp/src/pages/ObjectMenu.tsx
@@ -1,10 +1,20 @@
 import { NavBar } from "../components/NavBar";
+import TableComponent from "../components/TableComponent";
+import tables from "../virtualmodel/Tables";
 
-export function ObjectMenu(){
-    return(
-        <>
-        <NavBar/>
-        <div style={{textAlign:'center', fontSize:'60px', paddingTop:"40px"}}> Tables would go under here</div>
-        </>
-    );
+export function ObjectMenu() {
+  return (
+    <>
+      <NavBar />
+      <div
+        style={{ textAlign: "center", fontSize: "60px", paddingTop: "40px" }}
+      >
+        {" "}
+        Tables would go under here
+      </div>
+      <div>
+        <TableComponent columnNames={tables}></TableComponent>
+      </div>
+    </>
+  );
 }
