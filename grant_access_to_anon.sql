2fbcdea8727273e36903d515aae22827bf44178e
William Tremblay <wt22@videotron.ca>
2023-10-25 22:31:35 -0400

    #40 Exposed application_config schema to postgREST

--- grant_access_to_anon.sql
+++ grant_access_to_anon.sql
@@ -2,5 +2,8 @@
 GRANT SELECT ON application.tools TO web_anon;
 GRANT SELECT ON application.customers TO web_anon;
 GRANT SELECT ON application.rentals TO web_anon;
+GRANT USAGE ON SCHEMA application_config TO web_anon;
+GRANT SELECT ON application_config.properties TO web_anon;
+GRANT SELECT ON application_config.values TO web_anon;
 
 -- Note that this is a temporary solution, and will be replace by proper authentication later on
