90ad559b80ff407d13001e57e85b1fd044961209
Lyuba Georgieva <lyubageorgieva@hotmail.com>
2023-10-26 13:45:18 -0400

    Solving conflicts regarding the routes

--- UInnovateApp/src/App.tsx
+++ UInnovateApp/src/App.tsx
@@ -1,7 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { NavBar } from './components/NavBar';
-import { ObjectMenu } from './pages/ObjectMenu';
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NavBar } from "./components/NavBar";
+import { ObjectMenu } from "./pages/ObjectMenu";
 import { Element } from "./pages/Element";
 import { ListView } from './pages/ListView';
 import { EnumView } from './pages/EnumView';
@@ -9,7 +9,7 @@ import { EnumView } from './pages/EnumView';
 
 function App() {
   return (
-      <Routes>
+    <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/check" element={<NavBar />} />
       <Route path="/app" element={<ListView />} />
