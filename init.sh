de7264533575807b67ce8018c53ec2ab9a4fe23c
William Tremblay <wt22@videotron.ca>
2023-10-25 22:08:43 -0400

    #40 refactored init.sh to use env variable for db name

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 # Creating the application_config schema and tables
-psql -U ${POSTGRES_USER} -d uinnovate_test_db -a -f /SQL/create_app_config.sql
+psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -a -f /SQL/create_app_config.sql
 # Restoring the application data
-pg_restore -U ${POSTGRES_USER} -d uinnovate_test_db /dumps/uinnovate-test-db-dump.backup -n application -c --if-exists --no-owner --no-privileges 
+pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DB} /dumps/uinnovate-test-db-dump.backup -n application -c --if-exists --no-owner --no-privileges 
