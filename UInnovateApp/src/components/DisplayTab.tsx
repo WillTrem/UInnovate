e986a93560cdf8de63d0adf82dffe60c729700e6
William Tremblay <wt22@videotron.ca>
2023-11-03 12:25:03 -0400

    #19 added table selection tab in display settings

--- UInnovateApp/src/components/DisplayTab.tsx
+++ UInnovateApp/src/components/DisplayTab.tsx
@@ -1,6 +1,10 @@
 import Button from "react-bootstrap/Button";
 import { useTableVisibility } from "../contexts/TableVisibilityContext";
 import { TableItem } from "./TableItem";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 export const DisplayTab = () => {
 	const { tableVisibility, setTableVisibility } = useTableVisibility();
 
@@ -13,11 +17,37 @@ export const DisplayTab = () => {
 			toggleVisibility={setTableVisibility}
 		/>
 	));
-
+		console.log(tableItems)
 	return (
 		<div>
 			<div className='customization-title'>Tables</div>
-			<div className='table-list'>{tableItems}</div>
+			<Tab.Container>
+				<Row>
+					<Col sm={3}>
+						<Nav variant='pills' className="flex-column">
+							{Object.keys(tableVisibility).map((tableName) => {
+							  return (
+							  <Nav.Item key={tableName}>
+								<Nav.Link eventKey={tableName}>{tableName}</Nav.Link>
+							  </Nav.Item>)
+							})}
+						</Nav>
+					</Col>
+					<Col sm = {9}>
+					<Tab.Content>
+						{tableItems.map((tableItem) => {
+							const tableName = tableItem.props.tableName;
+						  return(
+							<Tab.Pane key = {tableName} eventKey={tableName}>
+								{tableItem}
+							</Tab.Pane>
+						  )
+						})}
+					</Tab.Content>
+					</Col>
+				</Row>
+			</Tab.Container>
+			{/* <div className='table-list'>{tableItems}</div> */}
 			<Button variant='primary' className='save-button'>
 				Save Changes
 			</Button>
