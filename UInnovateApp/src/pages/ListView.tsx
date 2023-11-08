072a1457680082b8042cbad88a9082ad3111916f
Samy Refik <samy.refik@gmail.com>
2023-11-08 18:30:24 -0500

    Modified tables and list view further

--- UInnovateApp/src/pages/ListView.tsx
+++ UInnovateApp/src/pages/ListView.tsx
@@ -1,8 +1,16 @@
 import { NavBar } from "../components/NavBar";
+import { useState } from "react";
 import attr from "../virtualmodel/Tables";
+import fetchData from "../virtualmodel/Tables";
 import TableTitles from "../components/TableTitles";
 
 export function ListView() {
+  const [attrData, setAttrData] = useState([]);
+
+  const onDataFetched = (data) => {
+    setAttrData(data);
+  };
+
   return (
     <>
       <NavBar />
@@ -14,6 +22,7 @@ export function ListView() {
       </div>
 
       <div>
+        <fetchData onDataFetched={onDataFetched} />
         <TableTitles attr={attr}></TableTitles>
       </div>
     </>
