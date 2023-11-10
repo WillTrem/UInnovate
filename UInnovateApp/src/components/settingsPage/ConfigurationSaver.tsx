a1a837524145b66840beefaf659fedd9241a6a19
William Tremblay <wt22@videotron.ca>
2023-11-09 22:48:41 -0500

    #19 Animation on save to database

--- UInnovateApp/src/components/settingsPage/ConfigurationSaver.tsx
+++ UInnovateApp/src/components/settingsPage/ConfigurationSaver.tsx
@@ -1,13 +1,18 @@
-import { useConfig } from "../../contexts/ConfigContext";
-import { useEffect, useRef } from 'react';
+import { ConfigType, useConfig } from "../../contexts/ConfigContext";
+import { useEffect, useRef, useState } from 'react';
 import { updateAppConfigValues } from '../../virtualmodel/Config';
+import { CircularProgress, Snackbar } from "@mui/material";
+import BuildIcon from '@mui/icons-material/Build';
+import { grey } from "@mui/material/colors";
 
 const CONFIG_UPDATE_TIMER_MS = 10000;
+const CONFIG_SAVE_ANIMATION_DURATION_MS = 1000;
 
 // Invisible component in charge of saving the configuration at regular intervals and on leave
 const ConfigurationSaver: React.FC = () => {
 	const { config } = useConfig();
-	const configRef = useRef(config);
+	const configRef = useRef<ConfigType>(config);
+	const [isSnackbarVisible, setSnackbarVisible] = useState(false);
 
 	// Keeps the configRef up to date with the latest config value
 	useEffect(() => {
@@ -18,19 +23,46 @@ const ConfigurationSaver: React.FC = () => {
 	// Initializes the timer and the cleanup functions
 	useEffect(() => {
 		const interval = setInterval(() => {
-			updateAppConfigValues(configRef.current)
-			console.log("Saving the configuration in the DB via timer.")
+			updateAppConfigValues(configRef.current);
+			setSnackbarVisible(true);
+			console.log("Saving the configuration in the DB via timer.");
 		}, CONFIG_UPDATE_TIMER_MS);
 
 		// Cleanup function
 		return () => {
 			clearInterval(interval);
+			setSnackbarVisible(true);
 			updateAppConfigValues(configRef.current);
-			console.log("Unmounting ConfigurationSaver, saving the configuration to the DB.")
+			console.log("Unmounting ConfigurationSaver, saving the configuration to the DB.");
 		}
 	}, []);
 
-	return <></>
+	function handleSnackbarClose(event?: React.SyntheticEvent | Event, reason?: string) {
+		if (reason === 'clickaway') {
+			return;
+		}
+
+		setSnackbarVisible(false);
+	}
+	return <>
+		<Snackbar open={isSnackbarVisible} autoHideDuration={CONFIG_SAVE_ANIMATION_DURATION_MS}
+			onClose={handleSnackbarClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+			<div>
+				<BuildIcon sx={{
+					color: grey[400]
+				}} />
+				<CircularProgress
+					sx={{
+						color: grey[400],
+						position: 'absolute',
+						top: -5,
+						left: -8,
+						zIndex: 1,
+					}}
+				/>
+			</div>
+		</Snackbar>
+	</>
 }
 
 export default ConfigurationSaver;
