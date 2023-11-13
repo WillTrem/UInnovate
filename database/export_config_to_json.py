8c68c13668056e8b8dc7a7e5b54c3b2eaa2b4f3a
alex <mariaalexandra.zana@brp.com>
2023-11-13 16:30:56 -0500

    export config file updated -az

--- database/export_config_to_json.py
+++ database/export_config_to_json.py
@@ -2,21 +2,15 @@
 ####TODO: invoke the procedure in meta.sql through a PostgREST API call
 ### make a new GET request to the appropriate endpoint to get the JSON file 
 
-import psycopg2 ##change this to PostgREST instead
+import requests
 import json
 
-# Connect to your PostgreSQL database
-conn = psycopg2.connect("dbname=db user=zanaalex password=turtle")
-cur = conn.cursor()
+# TODO: change this url for the actual PostgREST endpoint
+url = "http://localhost:30000/export_appconfig_to_json" ##not the right url yet
 
-# Call the SQL function
-cur.execute("SELECT export_appconfig_to_json()")
-result = cur.fetchone()[0]
+response = requests.get(url)
+data = response.json()
 
 # Write the result to a JSON file
 with open('appconfig_values.json', 'w') as json_file:
-    json.dump(result, json_file)
-
-# Close the connection
-cur.close()
-conn.close()
+    json.dump(data, json_file)
