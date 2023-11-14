495ec2ad5dbbb35322b8635f99ca07679fd68ce7
alex <mariaalexandra.zana@brp.com>
2023-11-14 11:51:25 -0500

    Fixed ConfigFetch to iterate over all tables and cols -az

--- UInnovateApp/src/virtualmodel/ConfigFetch.ts
+++ UInnovateApp/src/virtualmodel/ConfigFetch.ts
@@ -1,52 +1,69 @@
 import axios from "axios";
-import { ConfigType } from "../contexts/ConfigContext";
 
-const appconfig_propertiesURL = "http://localhost:3000/appconfig_properties";
+// const appconfig_propertiesURL = "http://localhost:3000/appconfig_properties";
+const appconfig_valuesURL = "http://localhost:3000/appconfig_values";
+const tablesURL = "http://localhost:3000/tables";
+// const columnsURL = "http://localhost:3000/columns";
 
-interface AppConfigProperty {
+interface UserConfigProperty {
 	name: string;
 	default_value: string;
-	// other fields from appconfig_properties table if necessary like value_type
 }
 
-export async function fetchDefaultAppConfigValues(
-	defaultConfigValue: ConfigType
-): Promise<ConfigType> {
+interface TableConfig {
+	name: string;
+	columns: ColumnConfig[];
+}
+
+interface ColumnConfig {
+	name: string;
+	type: string;
+	default_value?: string;
+}
+
+async function fetchTableAndColumnConfig(): Promise<TableConfig[]> {
 	try {
-		const response = await axios.get<AppConfigProperty[]>(
-			appconfig_propertiesURL,
-			{
-				headers: {
-					"Content-Type": "application/json",
-					"Accept-Profile": "meta",
-				},
-			}
+		//fetching tables and default app config values
+		const { data: tables } = await axios.get<TableConfig[]>(tablesURL);
+		const { data: userConfigProperties } = await axios.get<
+			UserConfigProperty[]
+		>(
+			appconfig_valuesURL,
+			{ headers: { "Accept-Profile": "meta" } } // Assuming there is a profile for user configurations
 		);
 
-		const defaultValues = response.data;
-		//Solution 1 (better):Iterate over all of the tables and then columns and check if theres an entry for it in the config ; configwdefaults that would have default values as well as modified values
-		//Sol 2: from def value, initialize global state from configcontext, and apply for each element (object view),
-		//mergedCnfig needs to be deleted and replaced with Solution 1
-		const mergedConfig: ConfigType = defaultValues.map((defaultItem) => {
-			const existingItem = defaultConfigValue?.find(
-				(ec) => ec.property === defaultItem.name
-			);
-			if (existingItem) {
-				return { ...existingItem };
-			} else {
-				return {
-					property: defaultItem.name,
-					value: defaultItem.default_value,
-				};
-			}
-		});
+		// apply user-modified configurations over the defaults
+		// step 1; loop through each table
+		for (const table of tables) {
+			for (const column of table.columns) {
+				// step 2; check if the table has a default value
+				// find user-modified value for this column(if it exists)
+				const userConfig = userConfigProperties.find(
+					(property) => property.name === column.name
+				);
 
-		return mergedConfig;
+				// override default value with user-modified one (if it exists)
+				if (userConfig) {
+					column.default_value = userConfig.default_value;
+				}
+			}
+		}
+
+		//tables with updated configs
+		return tables;
 	} catch (error) {
 		console.error("Error fetching default app config values:", error);
 		if (axios.isAxiosError(error)) {
 			console.error("Axios error:", error.response);
 		}
-		return [];
+		throw error; // rethrow error
 	}
 }
+
+fetchTableAndColumnConfig()
+	.then((config) => {
+		console.log("Fetched configuration:", config);
+	})
+	.catch((error) => {
+		console.error("Error fetching configuration:", error);
+	});
