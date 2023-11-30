4da31f5bbd051ad6e2d18f80e1148e5e9308f9e0
alex <mariaalexandra.zana@brp.com>
2023-11-30 15:33:57 -0500

    Env vars tab iitial commit

---
+++ UInnovateApp/src/components/settingsPage/EnvVarsTab.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { DataAccessor, Row } from "../../virtualmodel/DataAccessor";
+import vmd from "../../virtualmodel/VMD";
+
+export const EnvVarsTab = () => {
+	const schema = vmd.getSchema("meta");
+};
