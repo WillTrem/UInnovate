7603af81c928b71f84886b10daaa2375024c5ee8
witnick <witnick2joseph@gmail.com>
2023-12-01 09:09:28 -0500

    fixed missing tables in my script

--- database/useCase2/application.sql
+++ database/useCase2/application.sql
@@ -1,4 +1,32 @@
--- Table to store service tickets
+-- SCHEMA: application
+
+DROP SCHEMA IF EXISTS application CASCADE ;
+
+CREATE SCHEMA IF NOT EXISTS application ;
+
+GRANT USAGE ON SCHEMA application TO web_anon;
+
+--  user information
+CREATE TABLE application.users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
+
+--  ticket categories
+CREATE TABLE application.ticket_categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+--  ticket priorities
+CREATE TABLE application.ticket_priorities (
+    priority_id SERIAL PRIMARY KEY,
+    priority_name VARCHAR(20) UNIQUE NOT NULL
+);
+
+--  service tickets
 CREATE TABLE application.service_tickets (
     ticket_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES application.users(user_id) NOT NULL,
@@ -9,10 +37,10 @@ CREATE TABLE application.service_tickets (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMPTZ,
     resolved_at TIMESTAMPTZ,
-    ticket_tags TEXT -- Corrected the type to TEXT
+    ticket_tags Text
 );
 
--- Table to store ticket comments
+--  ticket comments
 CREATE TABLE application.ticket_comments (
     comment_id SERIAL PRIMARY KEY,
     ticket_id INT REFERENCES application.service_tickets(ticket_id) NOT NULL,
@@ -21,7 +49,7 @@ CREATE TABLE application.ticket_comments (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- Table to store ticket assignments
+--  ticket assignments
 CREATE TABLE application.ticket_assignments (
     assignment_id SERIAL PRIMARY KEY,
     ticket_id INT REFERENCES application.service_tickets(ticket_id) NOT NULL,
@@ -29,10 +57,19 @@ CREATE TABLE application.ticket_assignments (
     assigned_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- Table to store system logs
+--  system logs
 CREATE TABLE application.system_logs (
     log_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES application.users(user_id),
     action_performed VARCHAR(100) NOT NULL,
     timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+
+GRANT ALL ON application.users TO web_anon;
+GRANT ALL ON application.ticket_categories TO web_anon;
+GRANT ALL ON application.ticket_priorities TO web_anon;
+GRANT ALL ON application.service_tickets TO web_anon;
+GRANT ALL ON application.ticket_comments TO web_anon;
+GRANT ALL ON application.ticket_assignments TO web_anon;
+GRANT ALL ON application.system_logs TO web_anon;
