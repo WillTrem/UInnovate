7385d978ca7292dfdad68c51f1f156f6624e69dc
MaximeJoanisse <maxjoanisse101@gmail.com>
2023-11-30 20:03:59 -0500

    Debugging for referencing table

--- database/meta.sql
+++ database/meta.sql
@@ -1,26 +1,18 @@
--- VIEWS
--- Creating the custom schema holding the views
-DROP SCHEMA IF EXISTS meta CASCADE ;
-CREATE SCHEMA IF NOT EXISTS meta ;
-
--- Creating the schema view
-CREATE OR REPLACE VIEW meta.schemas ("schema") AS 
-(
-    SELECT nspname
-    FROM pg_catalog.pg_namespace
-    WHERE nspname NOT LIKE 'pg_%' -- Filtering out postgres default schemas
-    AND nspname NOT LIKE 'information_schema' -- ^
-) ;
-    
--- Creating the table view
-CREATE OR REPLACE VIEW meta.tables ( "schema", "table" ) AS
-(
-    SELECT table_schema, table_name
-    FROM information_schema.tables
-    WHERE table_schema IN (SELECT * FROM meta.schemas)
-    AND table_type = 'BASE TABLE'
+database\meta.sql
   ORDER BY table_schema
 ) ;
+
+
+
+-- Creating the constraints view
+CREATE OR REPLACE VIEW meta.constraints ("schema_name", "table_name", "column_name", "constraint_name") AS 
+(
+    SELECT ccu.table_schema, ccu.table_name, ccu.column_name, rc.constraint_name
+        FROM information_schema.referential_constraints AS rc
+        INNER JOIN  information_schema.constraint_column_usage AS ccu
+        USING (constraint_name)
+);
+
 
 -- Creating the columns view
 CREATE OR REPLACE VIEW meta.columns ("schema", "table", "column", "references_table") AS 
@@ -33,6 +25,7 @@ CREATE OR REPLACE VIEW meta.columns ("schema", "table", "column", "references_ta
         INNER JOIN  information_schema.constraint_column_usage AS ccu
         USING (constraint_name)
     ) AS rc
+    LEFT JOIN meta.constraints AS rc
     ON c.column_name = rc.column_name AND rc.constraint_name LIKE c.table_name || '%'
     WHERE c.table_name IN 
     (
@@ -41,8 +34,14 @@ CREATE OR REPLACE VIEW meta.columns ("schema", "table", "column", "references_ta
     ORDER BY c.table_schema, c.table_name
 ) ;
 
+
+
 -- TABLES
+
+
+
 -- Creating the application config properties table
+
 CREATE TABLE IF NOT EXISTS meta.appconfig_properties (
 	name TEXT PRIMARY KEY,
 	description TEXT,
@@ -50,6 +49,7 @@ CREATE TABLE IF NOT EXISTS meta.appconfig_properties (
 	default_value TEXT NOT NULL
 );
 -- Creating the application config values table
+
 CREATE TABLE IF NOT EXISTS meta.appconfig_values (
 	id SERIAL PRIMARY KEY,
 	"table" TEXT,
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS meta.appconfig_values (
 	ON UPDATE CASCADE,
     UNIQUE NULLS NOT DISTINCT (property, "table", "column")
 );
-
 -- Creating the scripts table
+
 CREATE TABLE IF NOT EXISTS meta.scripts (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
@@ -74,27 +74,25 @@ CREATE TABLE IF NOT EXISTS meta.scripts (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 -- USAGE 
+
+
 GRANT USAGE ON SCHEMA meta TO web_anon;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA meta TO web_anon;
-
 GRANT SELECT, UPDATE, INSERT ON meta.schemas TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.tables TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.columns TO web_anon;
+GRANT SELECT, UPDATE, INSERT ON meta.constraints TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.appconfig_properties TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.appconfig_values TO web_anon;
-GRANT SELECT, UPDATE, INSERT ON TABLE information_schema.constraint_column_usage TO web_anon;
-GRANT SELECT, UPDATE, INSERT ON TABLE information_schema.columns TO web_anon;
-GRANT SELECT, UPDATE, INSERT ON TABLE information_schema.referential_constraints TO web_anon;
-GRANT SELECT, UPDATE, INSERT ON TABLE information_schema.constraint_column_usage TO web_anon;
-GRANT SELECT, UPDATE, INSERT ON TABLE meta.tables TO web_anon;
-GRANT SELECT, UPDATE, INSERT ON TABLE meta.columns TO web_anon;
 
-GRANT DELETE ON meta.appconfig_values TO web_anon;
-GRANT EXECUTE ON FUNCTION meta.export_appconfig_to_json() TO web_anon;
-GRANT SELECT ON meta.columns TO web_anon;
-GRANT INSERT, UPDATE, DELETE ON meta.columns TO web_anon;
 
+
+GRANT USAGE ON SCHEMA information_schema TO web_anon;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA information_schema TO web_anon;
+GRANT SELECT ON information_schema.referential_constraints TO web_anon;
+GRANT SELECT ON information_schema.constraint_column_usage TO web_anon;
 -- EXPORT FUNCTIONALITY
+
 CREATE OR REPLACE FUNCTION meta.export_appconfig_to_json()
 RETURNS json  -- Specify the return type here
 LANGUAGE plpgsql
@@ -115,22 +113,22 @@ BEGIN
         SELECT name, description, value_type, default_value
         FROM meta.appconfig_properties
     ) ap ON TRUE;
-
     RETURN result;
 END;
 $BODY$;
 
+
+-- IMPORT FUNCTIONALITY
 -- USAGE 
 GRANT USAGE ON SCHEMA meta TO web_anon;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA meta TO web_anon;
-
 GRANT SELECT, UPDATE, INSERT ON meta.schemas TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.tables TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.columns TO web_anon;
+GRANT SELECT, UPDATE, INSERT ON meta.constraints TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.appconfig_properties TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.appconfig_values TO web_anon;
 GRANT SELECT, UPDATE, INSERT ON meta.scripts TO web_anon;
-
 GRANT EXPORT ON meta.export_appconfig_to_json TO web_anon;
 GRANT ALL ON meta.appconfig_properties TO web_anon;
 GRANT ALL ON meta.appconfig_values TO web_anon;
