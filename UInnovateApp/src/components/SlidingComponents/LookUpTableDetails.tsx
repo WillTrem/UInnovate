177c9f0b124b36f8f8269c77e6521c3fab4f546a
MaximeJoanisse <maxjoanisse101@gmail.com>
2023-12-28 16:35:33 -0500

    Removed Confusing Lines
    -Still isn't working perfectly but more clear for debugging

--- UInnovateApp/src/components/SlidingComponents/LookUpTableDetails.tsx
+++ UInnovateApp/src/components/SlidingComponents/LookUpTableDetails.tsx
@@ -16,16 +16,11 @@ const LookUpTableDetails: React.FC<TableListViewProps> = ({ table }: TableListVi
   }
   else {
     const [Columns, setColumns] = useState<Column[][]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [connect, setConnect] = useState<string>("");
     const [rows, setRows] = useState<Row[][] | undefined>([]);
     const getTable = JSON.parse(Local!);
     var count = Object.keys(getTable).length - 1;
-    let currentPrimaryKey: string;
-    const nonEditableColumn = table.columns.find((column: { is_editable: boolean; }) => column.is_editable === false);
-    if (nonEditableColumn) {
-      currentPrimaryKey = nonEditableColumn.column_name;
-    }
+
+
     const LookUpTables = (num: number) => {
       num = num - 1;
 
@@ -34,35 +29,26 @@ const LookUpTableDetails: React.FC<TableListViewProps> = ({ table }: TableListVi
       }
       else {
         const tableName = getTable[num];
-          const search = table.getColumns()
+        const search = table.getColumns()
 
-          let toolsColumn: string = "none";
-          search?.map((attribute) => {
-            if (attribute.references_table == tableName) {
-              toolsColumn = attribute.column_name;
-            }
-            else {
-              return (<>nothing</>);
-            }
-          });
-          const temp = toolsColumn + "L";
-          const connectionID = localStorage.getItem(temp);
-          if (connectionID == undefined) {
+        let toolsColumn: string = "";
+        search?.map((attribute) => {
+          if (attribute.references_table == tableName) {
+            toolsColumn = attribute.column_name;
+          }
+          else {
             return (<>nothing</>);
           }
-          localStorage.setItem('connect', connectionID);
-          useEffect(() => {
-            console.log("connectionId")
-            console.log(connectionID)
-            setConnect(connectionID);
-            console.log(connect)
-          }, []);
-          console.log(connect)
+        });
+        const temp = toolsColumn + "L";
+        const connectionID = localStorage.getItem(temp);
+        if (connectionID == undefined) {
+          return (<>nothing</>);
+        }
 
 
         useEffect(() => {
-          setLoading(true);
-          
+
           const getRows = async () => {
             const schemaObj = vmd.getTableSchema(tableName)
             if (!schemaObj) {
@@ -78,30 +64,29 @@ const LookUpTableDetails: React.FC<TableListViewProps> = ({ table }: TableListVi
               schemaObj.schema_name,
               tableName,
               toolsColumn,
-              "1"
+              connectionID
             );
             console.log(data_accessor);
             const lines = await data_accessor.fetchRows();
             if (!lines) {
               return console.error("Could not fetch rows Damn...");
             }
-            setRows(prevRows => [...prevRows, lines]);
+            // Filter the rows to only include the visible columns
+            const filteredRows = lines?.map((row) => {
+              const filteredRowData: { [key: string]: string | number | boolean } =
+                {};
+              Colss.forEach((column) => {
+                filteredRowData[column.column_name] = row[column.column_name];
+              });
+              return (filteredRowData);
+            });
+            setRows(prevRows => [...prevRows, filteredRows]);
 
           };
           getRows();
-        }, [num]);
+        }, []);
 
-        useEffect(() => {
-          setLoading(false);
-        }, [Columns, rows]);
-
-        if (loading) {
-          return (
-            <div>
-              <p>Loading...</p>
-            </div>
-          );
-        }
+       
         return (
           <div>
             Here is the connectionID: {connectionID}
@@ -109,27 +94,30 @@ const LookUpTableDetails: React.FC<TableListViewProps> = ({ table }: TableListVi
             <div>
               {/* Here is the table name: {toolsColumn} */}
             </div>
-            <TableComponent striped bordered hover>
-              <thead>
-                <tr>
-                  {Columns[num + 1].map((column) => {
-                    return <th key={column.column_name}>{column.column_name}</th>;
-                  })}
-                </tr>
-              </thead>
-              <tbody>
-      {Array.isArray(rows[num + 1]) && rows[num + 1].map((row, rowIdx) => (
-        <tr key={rowIdx}>
-          {Object.values(row).map((cell, idx) => (
-            <td key={idx}>
-              {cell && typeof cell !== 'object' ? cell.toString() : ''}
-            </td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
-            </TableComponent>
-
+            {Columns[num + 1] && rows[num + 1] ? (
+              <TableComponent striped bordered hover>
+                <thead>
+                  <tr>
+                    {Columns[num + 1].map((column) => {
+                      return <th key={column.column_name}>{column.column_name}</th>;
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.isArray(rows[num + 1]) && rows[num + 1].map((row, rowIdx) => (
+                    <tr key={rowIdx}>
+                      {Object.values(row).map((cell, idx) => (
+                        <td key={idx}>
+                          {cell && typeof cell !== 'object' ? cell.toString() : ''}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </TableComponent>
+            ) : (
+              <p>Loading...</p>
+            )}
 
           </div>
 
