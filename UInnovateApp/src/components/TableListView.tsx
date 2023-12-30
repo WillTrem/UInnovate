e304e3a9c3ec2fde2a2b15148accd0b68b966b36
AfandiM <84986555+AfandiM@users.noreply.github.com>
2023-12-29 23:59:14 -0500

    Added various ways to display columns, some bugs prevent full integration, but still functional

--- UInnovateApp/src/components/TableListView.tsx
+++ UInnovateApp/src/components/TableListView.tsx
@@ -2,25 +2,49 @@ import "../styles/TableComponent.css";
 import TableComponent from "react-bootstrap/Table";
 import vmd, { Table, Column } from "../virtualmodel/VMD";
 import { DataAccessor, Row } from "../virtualmodel/DataAccessor";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SlidingPanel from "react-sliding-side-panel";
 import "react-sliding-side-panel/lib/index.css";
-import { useConfig } from "../contexts/ConfigContext";
 import { ConfigProperty } from "../virtualmodel/ConfigProperties";
-import { NumericFormat } from "react-number-format";
-import { DateField } from "@mui/x-date-pickers/DateField";
+import StarterKit from "@tiptap/starter-kit";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import RRow from 'react-bootstrap/Row';
-import CCol from 'react-bootstrap/Col';
+import RRow from "react-bootstrap/Row";
+import CCol from "react-bootstrap/Col";
 import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-import { Switch, Button, Typography, Select, MenuItem, FormControl, FormHelperText, SelectChangeEvent } from "@mui/material";
+import {
+  Switch,
+  Button,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  FormHelperText,
+  SelectChangeEvent,
+} from "@mui/material";
 import AddRowPopup from "./AddRowPopup";
-import Pagination from '@mui/material/Pagination';
+import Pagination from "@mui/material/Pagination";
 
 import LookUpTableDetails from "./SlidingComponents/LookUpTableDetails";
 import { current } from "@reduxjs/toolkit";
 import { Container } from "react-bootstrap";
+import "../styles/TableListViewStyle.css";
+import {
+  LocalizationProvider,
+  StaticDateTimePicker,
+} from "@mui/x-date-pickers";
+import { CategoriesDisplayType } from "../virtualmodel/Config";
+import { MuiTelInput } from "mui-tel-input";
+import {
+  MenuButtonBold,
+  MenuButtonItalic,
+  MenuControlsContainer,
+  MenuDivider,
+  MenuSelectHeading,
+  RichTextEditor,
+  type RichTextEditorRef,
+} from "mui-tiptap";
 
 interface TableListViewProps {
   table: Table;
@@ -37,7 +61,6 @@ const inputStyle = {
   flexDirection: "column", // This will make the children (input elements) stack vertically
   alignItems: "flex-start",
   width: "65%",
-
 };
 
 const TableListView: React.FC<TableListViewProps> = ({
@@ -47,12 +70,15 @@ const TableListView: React.FC<TableListViewProps> = ({
 }) => {
   const [columns, setColumns] = useState<Column[]>([]);
   const [rows, setRows] = useState<Row[] | undefined>([]);
-  const { config } = useConfig();
   const [isPopupVisible, setIsPopupVisible] = useState<boolean>(false);
   const [inputValues, setInputValues] = useState<Row>({});
-  const [currentPrimaryKey, setCurrentPrimaryKey] = useState<string | null>(null);
-  const defaultOrderValue = table.columns.find(column => column.is_editable === false)?.column_name;
-  const [OrderValue, setOrderValue] = useState(defaultOrderValue || '');
+  const [currentPrimaryKey, setCurrentPrimaryKey] = useState<string | null>(
+    null
+  );
+  const defaultOrderValue = table.columns.find(
+    (column) => column.is_editable === false
+  )?.column_name;
+  const [OrderValue, setOrderValue] = useState(defaultOrderValue || "");
   const [PaginationValue, setPaginationValue] = useState<number>(50);
   const [PageNumber, setPageNumber] = useState<number>(1);
   const [Plength, setLength] = useState<number>(0);
@@ -60,10 +86,9 @@ const TableListView: React.FC<TableListViewProps> = ({
   const name = table.table_name + "T";
   const Local = localStorage.getItem(name);
   if (Local == null) {
-    const nulll = Local
+    const nulll = Local;
   }
   const getTable = JSON.parse(Local!);
-
 
   const getRows = async () => {
     const attributes = table.getVisibleColumns();
@@ -90,8 +115,7 @@ const TableListView: React.FC<TableListViewProps> = ({
 
     // Filter the rows to only include the visible columns
     const filteredRows = lines?.map((row) => {
-      const filteredRowData: { [key: string]: string | number | boolean } =
-        {};
+      const filteredRowData: { [key: string]: string | number | boolean } = {};
       attributes.forEach((column) => {
         filteredRowData[column.column_name] = row[column.column_name];
       });
@@ -103,75 +127,110 @@ const TableListView: React.FC<TableListViewProps> = ({
   };
 
   useEffect(() => {
-
     getRows();
   }, [table, OrderValue, PageNumber, PaginationValue]);
 
   const [openPanel, setOpenPanel] = useState(false);
   const [currentRow, setCurrentRow] = useState<Row>(new Row({}));
+  const [currentPhone, setCurrentPhone] = useState<string>("");
   const [inputField, setInputField] = useState<(column: Column) => JSX.Element>(
     () => <></>
   );
-
   const schema = vmd.getSchema("meta");
   const script_table = vmd.getTable("meta", "scripts");
+  const config_table = vmd.getTable("meta", "appconfig_values");
   const [scripts, setScripts] = useState<Row[] | undefined>([]);
+  const [appConfigValues, setAppConfigValues] = useState<Row[] | undefined>([]);
+  const rteRef = useRef<RichTextEditorRef>(null);
 
   const getScripts = async () => {
     if (!schema || !script_table) {
       throw new Error("Schema or table not found");
     }
 
-    const data_accessor: DataAccessor = vmd.getRowsDataAccessor(
+    const scripts_data_accessor: DataAccessor = vmd.getRowsDataAccessor(
       schema?.schema_name,
       script_table?.table_name
     );
 
-    const scripts_rows = await data_accessor?.fetchRows();
+    const scripts_rows = await scripts_data_accessor?.fetchRows();
     const filteredScripts = scripts_rows?.filter(
       (script) => script.table_name === table.table_name
     );
+
     setScripts(filteredScripts);
+  };
+
+  const getConfigs = async () => {
+    if (!schema || !config_table) {
+      throw new Error("Schema or table not found");
+    }
+
+    const config_data_accessor: DataAccessor = vmd.getRowsDataAccessor(
+      schema?.schema_name,
+      config_table?.table_name
+    );
+
+    const config_rows = await config_data_accessor?.fetchRows();
+
+    setAppConfigValues(config_rows);
   };
 
   useEffect(() => {
     getScripts();
-  }, []);
+    getConfigs();
+  }, [inputValues]);
 
   //For when order changes
   const handleOrderchange = (event: SelectChangeEvent) => {
     setOrderValue(event.target.value as string);
-
-  }
+  };
 
   //For when pagination limitm changes
   const handlePaginationchange = (event: SelectChangeEvent) => {
     setPaginationValue(event.target.value as number);
     setPageNumber(1);
-
-
-  }
+  };
 
   //For when page number changes
   const handlePageChange = (event, value) => {
     setPageNumber(value);
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const nonEditableColumn = table.columns.find(column => column.is_editable === false);
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    name: string,
+    isPhone: string
+  ) => {
+    const nonEditableColumn = table.columns.find(
+      (column) => column.is_editable === false
+    );
     if (nonEditableColumn) {
       setCurrentPrimaryKey(nonEditableColumn.column_name);
+    }
+    }
+    let eventName: string | undefined = undefined;
+    let eventValue: string | undefined = undefined;
+    if (event.target !== undefined) {
+      eventName = event.target.name;
+      eventValue = event.target.value;
+    } else {
+      if (isPhone !== undefined) {
+        eventName = name;
+        eventValue = event;
+      } else {
+        eventName = name;
+        eventValue = event.format();
+      }
     }
 
     setInputValues((prevInputValues) => ({
       ...prevInputValues,
-      [event.target.name]: event.target.value,
+      [eventName]: eventValue,
     }));
-
   };
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-
     event.preventDefault();
 
     const schema = vmd.getTableSchema(table.table_name);
@@ -179,7 +238,6 @@ const TableListView: React.FC<TableListViewProps> = ({
       console.error("Schema not found");
       return;
     }
-
     const storedPrimaryKeyValue = localStorage.getItem('currentPrimaryKeyValue');
 
 
@@ -193,49 +251,34 @@ const TableListView: React.FC<TableListViewProps> = ({
     data_accessor.updateRow().then(() => getRows());
     setInputValues({});
     setOpenPanel(false);
-    
   };
 
   // const ReadPrimaryKeyandValue = (Primekey:string, PrimekeyValue:string) => {
 
   // }
-
-
-
   useEffect(() => {
     const newInputField = (column: Column) => {
-      if (!config) {
+      if (!appConfigValues) {
         return null;
       }
-
-
-
-      const columnDisplayType = config.find(
+      const columnDisplayType = appConfigValues.find(
         (element) =>
           element.column == column.column_name &&
           element.table == table.table_name &&
           element.property == ConfigProperty.COLUMN_DISPLAY_TYPE
       );
 
-
-
-
-
-      if (
-        column.is_editable == false) {
-
-        localStorage.setItem("currentPrimaryKeyValue", currentRow.row[column.column_name]);
-
-      }
-      if (column.references_table != null) {
-        const string = column.column_name + "L"
-        localStorage.setItem(string, currentRow.row[column.column_name] as string);
+      if (column.is_editable == false) {
+        localStorage.setItem(
+          "currentPrimaryKeyValue",
+          currentRow.row[column.column_name]
+        );
       }
       if (!columnDisplayType || columnDisplayType.value == "text") {
         return (
           <input
             readOnly={column.is_editable === false ? true : false}
-            placeholder={String(currentRow.row[column.column_name]) || ""}
+            placeholder={String(currentRow?.row[column.column_name]) || ""}
             name={column.column_name}
             type="text"
             style={inputStyle}
@@ -244,11 +287,11 @@ const TableListView: React.FC<TableListViewProps> = ({
         );
       } else if (columnDisplayType.value == "number") {
         return (
-          <NumericFormat
+          <input
+            type="number"
+            name={column.column_name}
             readOnly={column.is_editable === false ? true : false}
             placeholder={String(currentRow.row[column.column_name]) || ""}
-            name={column.column_name}
-            type="text"
             style={inputStyle}
             onChange={handleInputChange}
           />
@@ -264,34 +307,116 @@ const TableListView: React.FC<TableListViewProps> = ({
             onChange={handleInputChange}
           />
         );
-      } else if (columnDisplayType.value == "datetime") {
-        return (
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateField
-              value={dayjs(currentRow.row[column.column_name] as string) || ""}
-              readOnly
-            />
-          </LocalizationProvider>
-        );
       } else if (columnDisplayType.value == "boolean") {
         return (
           <Switch
             checked={
-              currentRow.row[column.column_name] == "true"
+              currentRow?.row[column.column_name] == "true"
                 ? true
                 : false || false
             }
-            readOnly
+            name={column.column_name}
+          />
+        );
+      } else if (columnDisplayType.value == "date") {
+        return (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDatePicker
+              value={dayjs(currentRow.row[column.column_name])}
+              onChange={(date) => handleInputChange(date, column.column_name)}
+              name={column.column_name}
+              className="date-time-picker"
+              readOnly={column.is_editable === false ? true : false}
+            />
+          </LocalizationProvider>
+        );
+      } else if (columnDisplayType.value == "datetime") {
+        return (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDateTimePicker
+              value={dayjs(currentRow.row[column.column_name])}
+              onChange={(date) => handleInputChange(date, column.column_name)}
+              name={column.column_name}
+              className="date-time-picker"
+              readOnly={column.is_editable === false ? true : false}
+            />
+          </LocalizationProvider>
+        );
+      } else if (columnDisplayType.value == "categories") {
+        return (
+          <Select
+            value={"category1"}
+            onChange={handleInputChange}
+            className="popUp"
+          >
+            {Object.keys(CategoriesDisplayType).map((value) => (
+              <MenuItem key={value} value={value}>
+                {value}
+              </MenuItem>
+            ))}
+          </Select>
+        );
+      } else if (columnDisplayType.value == "phone") {
+        return (
+          <MuiTelInput
+            value={
+              currentPhone ? currentPhone : currentRow.row[column.column_name]
+            }
+            onChange={(phone) => {
+              handleInputChange(phone, column.column_name, "phone");
+              setCurrentPhone(phone);
+            }}
+            name={column.column_name}
+          />
+        );
+      } else if (columnDisplayType.value == "currency") {
+        return (
+          <div>
+            <input
+              type="number"
+              name={column.column_name}
+              className="input"
+              value={currentRow.row[column.column_name]}
+              onChange={handleInputChange}
+            />
+            <select value={"Hello"} onChange={onChangeCurrency}>
+              {currencyOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        );
+      } else if (columnDisplayType.value == "multiline_wysiwyg") {
+        return (
+          <RichTextEditor
+            name={column.column_name}
+            content={currentRow.row[column.column_name]}
+            onChange={handleInputChange}
+            ref={rteRef}
+            extensions={[StarterKit]} // Or any Tiptap extensions you wish!
+            // Optionally include `renderControls` for a menu-bar atop the editor:
+            renderControls={() => (
+              <MenuControlsContainer>
+                <MenuSelectHeading />
+                <MenuDivider />
+                <MenuButtonBold />
+                <MenuButtonItalic />
+                {/* Add more controls of your choosing here */}
+              </MenuControlsContainer>
+            )}
           />
         );
       }
     };
     setInputField(() => newInputField as (column: Column) => JSX.Element);
-  }, [currentRow, columns, config, table]);
+  }, [currentRow, columns, table, appConfigValues, currentPhone]);
 
   // Function to save the current row
   const handleOpenPanel = (row: Row) => {
     setCurrentRow(row);
+    setCurrentPhone("");
     if (!table.has_details_view) {
       return;
     }
@@ -348,26 +473,22 @@ const TableListView: React.FC<TableListViewProps> = ({
               </Button>
             );
           })}
-
         </div>
         <div>
           <FormControl size="small">
-            <h6 style={{ textAlign: 'left' }}>Ordering</h6>
+            <h6 style={{ textAlign: "left" }}>Ordering</h6>
             <Select
               value={OrderValue}
               displayEmpty
               onChange={handleOrderchange}
-
             >
               {table.columns.map((column, index) => (
                 <MenuItem value={column.column_name} key={index}>
                   {column.column_name}
                 </MenuItem>
               ))}
-
             </Select>
           </FormControl>
-
         </div>
       </div>
       <TableComponent striped bordered hover>
@@ -394,25 +515,22 @@ const TableListView: React.FC<TableListViewProps> = ({
           })}
         </tbody>
       </TableComponent>
-      <div >
+      <div>
         <Container>
-          <RRow  >
-            <CCol sm={5} className="mx-auto" style={{ textAlign: 'right' }}>
+          <RRow>
+            <CCol sm={5} className="mx-auto" style={{ textAlign: "right" }}>
               <Pagination
                 count={Math.ceil(Plength / PaginationValue)}
                 page={PageNumber}
                 onChange={handlePageChange}
               />
             </CCol>
-            <CCol sm={2} className="ml-auto" style={{ textAlign: 'right' }}>
-
+            <CCol sm={2} className="ml-auto" style={{ textAlign: "right" }}>
               <FormControl size="small">
                 <Select
                   value={PaginationValue}
                   displayEmpty
                   onChange={handlePaginationchange}
-
-
                 >
                   <MenuItem value={1}>1 per page</MenuItem>
                   <MenuItem value={5}>5 per page</MenuItem>
@@ -423,9 +541,7 @@ const TableListView: React.FC<TableListViewProps> = ({
               </FormControl>
             </CCol>
           </RRow>
-
         </Container>
-
       </div>
 
       <SlidingPanel
@@ -433,7 +549,9 @@ const TableListView: React.FC<TableListViewProps> = ({
         isOpen={openPanel}
         size={50}
         panelContainerClassName="panel-container"
-        backdropClicked={() => setOpenPanel(false)}
+        backdropClicked={() => {
+          setOpenPanel(false);
+        }}
       >
         <div className="form-panel-container">
           <Typography variant="h5">Details</Typography>
@@ -463,15 +581,20 @@ const TableListView: React.FC<TableListViewProps> = ({
             </Button>
             <Button
               variant="contained"
-              style={{ marginTop: 20, backgroundColor: "#403eb5", width: "fit-content", marginLeft: 10 }}
+              style={{
+                marginTop: 20,
+                backgroundColor: "#403eb5",
+                width: "fit-content",
+                marginLeft: 10,
+              }}
               onClick={handleFormSubmit}
             >
               Save
             </Button>
-
           </div>
-
         </div>
+
+        <LookUpTableDetails table={table} />
         {localStorage.getItem(table.table_name + "T") === null || getTable[-1] == "none"? (
           <div></div>
         ) : showTable ? (
