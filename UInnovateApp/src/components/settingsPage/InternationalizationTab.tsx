702aaee45a3ff37b903384dc2e85e8edd94f5518
Lyuba <74876532+lyubageorgieva@users.noreply.github.com>
2024-01-09 10:17:57 +0000

    Adding the logic to map out the i18n_translation view onto the Internationalization tab

--- UInnovateApp/src/components/settingsPage/InternationalizationTab.tsx
+++ UInnovateApp/src/components/settingsPage/InternationalizationTab.tsx
@@ -1,11 +1,11 @@
-import { Col, Row as Line, Tab, Nav } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import { Row as Line, Tab } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import { DataAccessor, Row } from "../../virtualmodel/DataAccessor";
-import vmd from "../../virtualmodel/VMD";
-import { ScriptEditor } from "./ScriptEditor";
+import vmd, { Schema } from "../../virtualmodel/VMD";
 import { Modal, Form } from "react-bootstrap";
 import { Button } from "@mui/material";
 import TableComponent from "react-bootstrap/Table";
+
 
 const buttonStyle = {
     marginRight: 10,
@@ -15,33 +15,41 @@ const buttonStyle = {
 
 
 const InternationalizationTab = () => {
-    const schema = vmd.getSchema("meta");
-    const script_view = vmd.getTable("meta", "i18n_translation");
-    const columns = script_view?.getColumns();
+    // const schema = vmd.getSchema("meta");
+    // const script_view = vmd.getTable("meta", "i18n_translation");
+    // const columns = script_view?.getColumns();
 
-    const [scripts, setScripts] = useState<Row[] | undefined>([]);
     const [showModal, setShowModal] = useState<boolean>(false);
     const [newScript, setNewScript] = useState<Row>({});
+    const [translations, setTranslations] = useState<Row[]>([]);
+    const [schemas, setSchemas] = useState<Schema[]>([]);
+    const [tables, setTables] = useState<string[]>([]);
+    const [storedProcedures, setStoredProcedures] = useState<string[]>([]);
 
-    // TODO: Properly fetch the view from the data accessor. Currently, the view is hardcoded.
-    const getScripts = async () => {
-    if (!schema || !script_view) {
-        return;
-    }
+	const getTranslations = async () => {
+		const data_accessor: DataAccessor = vmd.getViewRowsDataAccessor(
+			"meta",
+			"i18n_translation"
+		);
 
-    const data_accessor: DataAccessor = vmd.getRowsDataAccessor(
-        schema?.schema_name,
-        script_view?.table_name
-        
-    );
+		const rows = await data_accessor.fetchRows();
+		if (rows) {
+			setTranslations(rows);
+		}
+	}
 
-    const scripts_rows = await data_accessor?.fetchRows();
-    setScripts(scripts_rows);
+    const fetchSchemasAndTables = async () => {
+        const schemaObjects = vmd.getSchemas();
+        const tableNames = vmd.getAllTables().map((table) => table.table_name);
+    
+        setSchemas(schemaObjects);
+        setTables(tableNames);
     };
 
-    useEffect(() => {
-        getScripts();
-    });
+	useEffect(() => {
+		getTranslations();
+        fetchSchemasAndTables();
+	}, [])
 
     const handleAddLanguage = async () => {
         setShowModal(true);
@@ -59,7 +67,7 @@ const InternationalizationTab = () => {
         );
 
         await data_accessor?.addRow();
-        getScripts();
+
         setShowModal(false);
     };
     return (
@@ -107,7 +115,6 @@ const InternationalizationTab = () => {
                     </div>
                 </Modal.Footer>
             </Modal>
-   
         
             <Tab.Content>
             <h4>Internationalization</h4>
@@ -127,45 +134,58 @@ const InternationalizationTab = () => {
                     Refresh
                 </Button>
             </div>
-                <TableComponent bordered>
-                    <thead>
-                        <tr>
-                            <th>Label</th>
-                            <th>Lang:ENG</th>
-                            <th>Lang:FR</th>
-                        </tr>
-                        <tr>
-                            <th>label 1</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        <tr>
-                            <th>label 2</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        <tr>
-                            <th>label 3</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        <tr>
-                            <th>label 4</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-
-                    </thead>
-                    <tbody>
-                    </tbody>
-				</TableComponent>
-                
+            <TableComponent bordered>
+        <thead>
+        <tr>
+            <th>Label</th>
+            <th>Translation</th>
+        </tr>
+        </thead>
+        <tbody>
+            {translations && translations.map((translation, idx) => {
+                if (translation) {
+                    return (
+                    <TranslationTableRow
+                        key={idx}
+                        keyCode={translation["key_code"] as string}
+                        value={translation["value"] as string}
+                    />
+                );
+            }
+                return <React.Fragment key={idx} />;
+            })}
+          {/* Display schema names and table names in the key_code column */}
+            {schemas.map((schema, idx) => (
+                <TranslationTableRow
+                key={idx}
+                keyCode={schema.schema_name}
+                //   value={`Schema: ${schema.schema_name}`}
+                />
+            ))}
+            {tables.map((table, idx) => (
+                <TranslationTableRow key={idx} keyCode={table}  />
+            ))}
+        </tbody>
+        </TableComponent>
             </Tab.Content>
-        
         </Line>
     </Tab.Container>
     </div>
 );
 };
+
+interface TranslationTableRowProps {
+	keyCode?: string,
+	value?: string
+}
+const TranslationTableRow: React.FC<TranslationTableRowProps> = ({ keyCode, value }) => {
+	// const schemaNames = vmd.getSchemas().map((schema) => schema.schema_name);
+
+
+	return <tr>
+		<td>{keyCode}</td>
+		<td>{value}</td>
+	</tr >
+}
 
 export default InternationalizationTab;
