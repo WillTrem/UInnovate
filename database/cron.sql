eea4f00f32d7fe167efab90f788083bee1d85700
tsitirides <47327032+tsitirides@users.noreply.github.com>
2024-01-11 15:58:18 -0500

    Added functions
    added schedule/unschedule procedure functions to the cron schema to handle the scheduling and unscheduling calls from the scheduled activities page.

--- database/cron.sql
+++ database/cron.sql
@@ -7,3 +7,24 @@ ALTER POLICY cron_job_run_details_policy ON cron.job_run_details
 USING (username = current_user OR current_user = 'web_anon');
 ALTER POLICY cron_job_policy ON cron.job 
 USING (username = current_user OR current_user = 'web_anon');
+
+CREATE OR REPLACE FUNCTION cron.schedule_job_by_name(stored_procedure varchar, cron_schedule varchar)
+RETURNS varchar
+LANGUAGE plpgsql
+AS $BODY$
+BEGIN
+    EXECUTE format('SELECT cron.schedule(%L, %L, %L)', stored_procedure, cron_schedule, 'CALL ' || quote_ident(stored_procedure) || '()');
+    RETURN format('Successfully scheduled job %s', stored_procedure);
+END;
+$BODY$;
+
+
+CREATE OR REPLACE FUNCTION cron.unschedule_job_by_name(stored_procedure varchar)
+RETURNS varchar
+LANGUAGE plpgsql
+AS $BODY$
+BEGIN
+    EXECUTE format('SELECT cron.unschedule(%L)', stored_procedure);
+    RETURN format('Successfully unscheduled job %s', stored_procedure);
+END;
+$BODY$;
