ecb371d5dad0aae54a3d3bcc1e743a3b28306db4
AfandiM <84986555+AfandiM@users.noreply.github.com>
2024-01-26 12:37:22 -0500

    Added minor fixes to the tests

--- UInnovateApp/src/virtualmodel/__mocks__/DataAccessor.tsx
+++ UInnovateApp/src/virtualmodel/__mocks__/DataAccessor.tsx
@@ -3,31 +3,39 @@ import { Row } from "../DataAccessor";
 import { ConfigProperty } from "../ConfigProperties";
 
 export { Row };
+let mock_schema_name = "";
+let mock_table_name = "";
 export class DataAccessorMock {
-  constructor() {}
+  constructor(schema_name?, table_name?) {
+    mock_schema_name = schema_name;
+    mock_table_name = table_name;
+  }
 
   fetchRows = vi.fn().mockImplementation(() => {
     console.log("fetchRows in DataAccessor mock was called");
-    return Promise.resolve([
-      {
-        column: "Column1",
-        table: "Table1",
-        property: ConfigProperty.COLUMN_DISPLAY_TYPE,
-        value: "datetime",
-      },
-      {
-        column: "Column2",
-        table: "Table1",
-        property: ConfigProperty.COLUMN_DISPLAY_TYPE,
-        value: "date",
-      },
-      {
-        column: "Column3",
-        table: "Table1",
-        property: ConfigProperty.COLUMN_DISPLAY_TYPE,
-        value: "categories",
-      },
-    ]);
+    if (mock_schema_name == "meta" && mock_table_name == "appconfig_values") {
+      return Promise.resolve([
+        {
+          column: "Column1",
+          table: "Table1",
+          property: ConfigProperty.COLUMN_DISPLAY_TYPE,
+          value: "datetime",
+        },
+        {
+          column: "Column2",
+          table: "Table1",
+          property: ConfigProperty.COLUMN_DISPLAY_TYPE,
+          value: "date",
+        },
+        {
+          column: "Column3",
+          table: "Table1",
+          property: ConfigProperty.COLUMN_DISPLAY_TYPE,
+          value: "categories",
+        },
+      ]);
+    }
+    return Promise.resolve([]);
   });
 
   upsert = vi.fn().mockImplementation(() => {
