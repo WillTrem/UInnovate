0d0788424dfb6cf246ac5af9e8f706331f9a65c3
Samy Refik <samy.refik@gmail.com>
2024-01-29 11:16:47 -0500

    #104 Uninstalling isolated-vm, refactoring code to use node:vm

--- UInnovateApp/src/virtualmodel/ScriptHandler.ts
+++ UInnovateApp/src/virtualmodel/ScriptHandler.ts
@@ -1,36 +1,23 @@
-// You *must* import the extension in nodejs first.
-// import "../../node_modules/isolated-vm/out/isolated_vm.node";
-
-import * as ivm from "isolated-vm";
+import { createContext, runInContext } from "node:vm";
 
 class ScriptHandler {
-  private isolate: ivm.Isolate;
-  private context?: ivm.Context;
-  private jail?: ivm.Reference<Record<string | symbol | number, unknown>>;
+  private context: { [key: string]: unknown };
 
   constructor() {
-    this.isolate = new ivm.Isolate({ memoryLimit: 128 });
+    this.context = {};
+    createContext(this.context);
   }
 
-  public async initialize(): Promise<void> {
-    this.context = await this.isolate.createContext();
-    this.jail = this.context.global;
-    this.jail.setSync("global", this.jail.derefInto());
+  public runScript(script: string): void {
+    runInContext(script, this.context);
   }
 
-  public async runScript(script: string): Promise<unknown> {
-    if (!this.context || !this.jail) {
-      throw new Error("ScriptHandler is not initialized.");
-    }
-
-    const scriptObject = await this.isolate.compileScript(script);
-    const result = await scriptObject.run(this.context, { timeout: 1000 });
-    return result;
+  public getContext(): { [key: string]: unknown } {
+    return this.context;
   }
 }
 
 // Making a singleton instance of ScriptHandler
 const scriptHandler = new ScriptHandler();
-scriptHandler.initialize();
 
 export default scriptHandler;
