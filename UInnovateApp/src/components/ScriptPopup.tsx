dd3c9909e22f8a7fae85497d25ff83e8b82b7ca5
Samy Refik <samy.refik@gmail.com>
2024-01-30 17:08:49 -0500

    #104 Added two new popups upon script execution (failure and success script popups)

---
+++ UInnovateApp/src/components/ScriptPopup.tsx
@@ -0,0 +1,77 @@
+import { Modal, Box, Typography, Button } from "@mui/material";
+import "../styles/AddEnumModal.css";
+
+const style = {
+  position: "absolute",
+  margin: "auto",
+  padding: 3,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  bgcolor: "#f1f1f1",
+  border: "2px solid #000",
+  borderRadius: 8,
+  boxShadow: 24,
+  p: 4,
+};
+
+const buttonStyle = {
+  marginRight: 10,
+  backgroundColor: "#404040",
+};
+
+const ScriptErrorPopup = ({
+  onClose,
+  error,
+}: {
+  onClose: () => void;
+  error: string | Error;
+}) => {
+  return (
+    <Modal
+      open={true}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h5" component="h2">
+          Error
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          It seems that an error occurred while trying to execute the script.
+          {error.toString()}
+        </Typography>
+        <Button variant="contained" style={buttonStyle} onClick={onClose}>
+          Close
+        </Button>
+      </Box>
+    </Modal>
+  );
+};
+
+const ScriptSuccessPopup = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <Modal
+      open={true}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h5" component="h2">
+          Success
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Your script was successfully executed.
+        </Typography>
+        <Button variant="contained" style={buttonStyle} onClick={onClose}>
+          Close
+        </Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export { ScriptErrorPopup, ScriptSuccessPopup };
