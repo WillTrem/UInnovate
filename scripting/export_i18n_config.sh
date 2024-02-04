971c2a47ee591ca1e294811bd8fd9c739184fc18
Samy Refik <samy.refik@gmail.com>
2024-02-03 19:12:06 -0500

    #180 fixed the export script for i18n configs

--- scripting/export_i18n_config.sh
+++ scripting/export_i18n_config.sh
@@ -4,20 +4,17 @@ source ../.env
 # Define the output JSON file
 output_file="i18n_configurations.json"
 
+# Define the endpoint and the function to call
+POSTGREST_ENDPOINT="http://localhost:3000"
+FUNCTION_ENDPOINT="$POSTGREST_ENDPOINT/rpc/export_i18n_to_json"
+
 DB_CONTAINER_NAME="db"
 DB_USER=$POSTGRES_USER
 DB_PASS=$POSTGRES_PASSWORD
 DB_NAME=$POSTGRES_DB_NAME
 
-# 1st way not working
-# psql -h $DB_CONTAINER_NAME -U "$DB_USER" -d "$DB_NAME" -c "SELECT meta.export_i18n_to_json();" -t -o "$output_file"
-
-# 2nd way not working
-# docker exec -i $DB_CONTAINER psql -tA -U "$DB_USER" -d "$DB_NAME" -c "SELECT meta.export_i18n_to_json();" > "$output_file"
-
-# 3rd way not working 
-# psql -U $DB_USER -d $DB_NAME -t -c "SELECT meta.export_i18n_to_json();" > $output_file
+# Call the function directly
+curl -s "$FUNCTION_ENDPOINT" \
+ -X POST -H "Content-Profile: meta" -H "Content-Type: application/json" > "$output_file"
 
 echo "i18n configurations have been exported to $output_file"
-
-
