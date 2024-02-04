3c3d2a1a08550adeb97f12bda6faec88a653dcc1
Lyuba <74876532+lyubageorgieva@users.noreply.github.com>
2024-02-03 19:55:02 -0500

    Fixed the import to reflect the same process as the export, using a CURL request

--- scripting/import_i18n_from_json.sh
+++ scripting/import_i18n_from_json.sh
@@ -4,14 +4,13 @@ source ../.env
 # Define the input JSON file
 input_file="i18n_configurations.json"
 
-DB_CONTAINER="db"
-DB_USER=$POSTGRES_USER
-DB_PASS=$POSTGRES_PASSWORD
-DB_NAME=$POSTGRES_DB_NAME
+# Define the endpoint and the function to call
+POSTGREST_ENDPOINT="http://localhost:3000"
+FUNCTION_ENDPOINT="$POSTGREST_ENDPOINT/rpc/import_i18n_from_json"
 
-# Read the JSON file content
-json_data=$(<"$input_file")
+# Call the function directly
+curl -s "$FUNCTION_ENDPOINT" \
+    -X POST -H "Content-Profile: meta" -H "Content-Type: application/json" \
+    -d @"$input_file"
 
-docker exec -i $DB_CONTAINER psql -tA -U "$DB_USER" -d "$DB_NAME" -c "SELECT meta.import_i18n_from_json('$json_data')"
-
-echo "i18n configurations have been imported from $input_file" 
+echo "Import completed from $input_file. The configuration is saved in the database"
