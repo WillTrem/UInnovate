bafb5f721eb6ad44ce9e3b466aaa8ed56917b9f9
MariaR001 <ma_iv@live.concordia.ca>
2024-02-05 14:59:21 -0500

    added unit tests for LookUpTableDetails, DataAccessor and FunctionAccessor

---
+++ UInnovateApp/src/tests/components/slidingComponents/LookUpTableDetails.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expect } from "vitest";
+import LookUpTableDetails from '../../../components/SlidingComponents/LookUpTableDetails';
+
+// Mocking the vmd module
+import Table from "react-bootstrap/Table"; // Replace 'path/to/Table' with the actual path to the Table type
+
+const mockTable: typeof Table = {
+    lookup_tables: '{"1": "table1", "2": "table2"}',
+    getColumns: () => [],
+};
+
+const renderComponent = () => render(<LookUpTableDetails table={mockTable} table_name="" table_display_type="" is_visible={false} has_details_view={false} />);
+
+describe('LookUpTableDetails component', () => {
+  it('renders the component with loading state', async () => {
+    renderComponent();
+
+    await waitFor(() => {
+        const loadingElement = screen.getByText(/Loading.../i);
+    expect(loadingElement).toBeInTheDocument();
+  });
+});
+
+  it('renders the component with "Missing ConnectionID" state', async () => {
+    globalThis.fetch = async () => ({
+      json: async () => [{ /* Mock row data */ }],
+    } as Response);
+
+    renderComponent();
+
+    // Wait for the asynchronous operations to complete
+    await waitFor(() => {
+      // Check for the presence of "Missing ConnectionID"
+      const missingConnectionIDElement = screen.queryByText(/Missing ConnectionID/i);
+      expect(missingConnectionIDElement).toBeInTheDocument();
+    });
+  });
+
+  it('renders the component with data including "table1"', async () => {
+    // Mock necessary functions for useEffect
+    globalThis.fetch = async () => ({
+      json: async () => [{ /* Mock row data */ }],
+    } as Response);
+
+    renderComponent();
+
+    // Wait for the asynchronous operations to complete
+    await waitFor(() => {
+      // Check for the presence of "Missing ConnectionID"
+      const missingConnectionIDElement = screen.getByText(/Missing ConnectionID/i);
+      expect(missingConnectionIDElement).toBeInTheDocument();
+
+      // Assert that your component renders the expected content
+      const table1Element = screen.getByText((content) => {
+        // Use a custom text matcher function to handle dynamic content
+        const normalizedContent = content.trim().toLowerCase();
+        return normalizedContent.includes('table1');
+      });
+
+      expect(table1Element).toBeInTheDocument();
+    });
+  });
+});
