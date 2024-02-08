58d8093da6b91b3669dacbb0a74654aac4dd33dd
tsitirides <47327032+tsitirides@users.noreply.github.com>
2024-02-08 13:50:23 -0500

    export curl change
    using curl instead of docker exec, to stay more consistent with other teammates and is more beneficial to run it this way.

--- scripting/export_env_variables.sh
+++ scripting/export_env_variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source ../.env
 
+POSTGREST_ENDPOINT="http://localhost:3000"
+FUNCTION_ENDPOINT="$POSTGREST_ENDPOINT/rpc/export_env_vars_to_json"
+
 # Define the output JSON file
 output_file="db_env_variables.json"
 
-DB_CONTAINER="db"
-DB_USER=$POSTGRES_USER
-DB_PASS=$POSTGRES_PASSWORD
-DB_NAME=$POSTGRES_DB_NAME
-docker exec -i $DB_CONTAINER psql -tA -U "$DB_USER" -d "$DB_NAME" -c "SELECT meta.export_env_vars_to_json();" > "$output_file"
+curl -s "$FUNCTION_ENDPOINT" \
+    -X POST -H "Content-Profile: meta" -H "Content-Type: application/json" > "$output_file"
 
-echo "Cron configurations have been exported to $output_file"
+echo "Environment Variables have been exported to $output_file"
