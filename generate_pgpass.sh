e70151c1bd2c567df738a0a4f864f0643c8e1b8e
alex <zanaalexia@yahoo.com>
2024-02-08 17:58:12 -0500

    fixed generate_pgpass script

--- generate_pgpass.sh
+++ generate_pgpass.sh
@@ -4,6 +4,11 @@
 # the pgpass file content + sets the right permissions for it
 
 # Generate the pgpass file content using the .env variables
-echo "db:5432:db:${POSTGRES_USER}:${PGADMIN_DEFAULT_PASSWORD}" > /tmp/.pgpass
+# /Users/azana/UInnovate/.pgpass
 
-chmod 0600 /tmp/pgpass
+# echo "db:5432:db:${POSTGRES_USER}:${PGADMIN_DEFAULT_PASSWORD}" > "${PGPASS_FILE}"
+source "./.env"
+pwd
+echo "db:5432:${POSTGRES_DB_NAME}:${POSTGRES_USER}:${POSTGRES_PASSWORD}" > /tmp/.pgpass
+
+# chmod 0600 /tmp/pgpass
