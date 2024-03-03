b86f11b4f52cadc036147db642927d477c16e35a
MariaR001 <ma_iv@live.concordia.ca>
2024-03-03 13:43:21 -0500

    minor changes

--- UInnovateApp/src/components/settingsPage/UserLogs.tsx
+++ UInnovateApp/src/components/settingsPage/UserLogs.tsx
@@ -1,5 +1,3 @@
-//display the user logs, get data from user_logs table in db
-
 import { useEffect, useState } from "react";
 import axiosCustom from "../../api/AxiosCustom";
 import { DataGrid, GridColDef, GridRowId } from "@mui/x-data-grid";
@@ -17,31 +15,51 @@ const columns: GridColDef[] = [
 ];
 
 export default function UserLogs() {
-  const [rows, setRows] = useState<GridRowId[]>([]);
+  const [logs, setLogs] = useState<GridRowId[]>([]);
   const [users, setUsers] = useState<Row[]>([]);
 
-  const getUsers  = async () => {
-    const data_accessor: DataAccessor = vmd.getViewRowsDataAccessor(
-			"meta",
-			"user_info"
-		);
+  const getLogs = async () => {
+    try {
+      const response = await axiosCustom.get("/settings/userlogs"); 
+      const rows = response.data; 
+      const userlogs: GridRowId[] = rows.map((row, index) => ({
+        id: index,
+        timestamp: row.timestamp,
+        user_id: row.user_id,
+        action: row.action,
+        details: row.details,
+        schema_name: row.schema_name,
+        table_name: row.table_name,
+      }));
+      setLogs(userlogs);
+    } catch (error) {
+      console.error("Error fetching logs:", error);
+    }
+  };
 
-		const rows = await data_accessor.fetchRows();
-		if (rows) {
-			setUsers(rows);
-		}
-  } 
+  const getUsers = async () => {
+    try {
+      const data_accessor: DataAccessor = vmd.getViewRowsDataAccessor(
+        "meta",
+        "user_info"
+      );
+      const rows = await data_accessor.fetchRows();
+      if (rows) {
+        setUsers(rows);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
 
   useEffect(() => {
     getUsers();
-    axiosCustom.get("http://localhost:5173/api/logs").then((response) => {
-      setRows(response.data);
-    });
+    getLogs();
   }, []);
 
   return (
     <div style={{ height: 400, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} />
+      <DataGrid rows={logs} columns={columns} />
     </div>
   );
 }
