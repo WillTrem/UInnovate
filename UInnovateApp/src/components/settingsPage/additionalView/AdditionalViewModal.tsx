acd3af088579aeff24d9b0bb2546b29099a7475a
witnick <witnick2joseph@gmail.com>
2024-03-09 00:06:17 -0500

    - Refactor Additional View navigation bar to update the selected view type - Show the selected view type in the tab pane (section in red box) - Make selected view type be the default on first load - Moved  ViewTypeEnum to enum folder

--- UInnovateApp/src/components/settingsPage/additionalView/AdditionalViewModal.tsx
+++ UInnovateApp/src/components/settingsPage/additionalView/AdditionalViewModal.tsx
@@ -1,119 +1,141 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
-import { Modal , Form} from 'react-bootstrap'
-import { ViewTypeEnum } from './ViewTypeEnum';
-import { insertNewView } from '../../../virtualmodel/AdditionalViewsDataAccessor';
-import vmd, { Table } from '../../../virtualmodel/VMD';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../redux/Store';
+import { Modal, Form } from "react-bootstrap";
+import { ViewTypeEnum } from "../../../enums/ViewTypeEnum";
+import { insertNewView } from "../../../virtualmodel/AdditionalViewsDataAccessor";
+import vmd, { Table } from "../../../virtualmodel/VMD";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/Store";
 
-interface AdditionalViewModalProp{
-    show: boolean;
-    setShow: (v:boolean)=>void;
-    refreshList:()=>void;
+interface AdditionalViewModalProp {
+  show: boolean;
+  setShow: (v: boolean) => void;
+  refreshList: () => void;
 }
 
-const AdditionalViewModal = ({show, setShow, refreshList: updateList}:AdditionalViewModalProp) => {
-    const [viewName, setViewName] = useState<string>('');
-    const [viewType, setViewType] = useState<number>(1);
-    const [tableName, setTableName] = useState<string>('');
-    const [customCode, setCustomCode] = useState<string>('');
-    const [tableList, setTableList]= useState<Table[]>([]);
+const AdditionalViewModal = ({
+  show,
+  setShow,
+  refreshList: updateList,
+}: AdditionalViewModalProp) => {
+  const [viewName, setViewName] = useState<string>("");
+  const [viewType, setViewType] = useState<number>(1);
+  const [tableName, setTableName] = useState<string>("");
+  const [customCode, setCustomCode] = useState<string>("");
+  const [tableList, setTableList] = useState<Table[]>([]);
 
-     const schemaName = useSelector((state: RootState) => state.schema.value);
+  const schemaName = useSelector((state: RootState) => state.schema.value);
 
-    useEffect(()=>{
-        // Only show tables of the selected schema
-        const tables = vmd.getSchema(schemaName)?.tables || [];
-        setTableList(tables);
-    }, [schemaName]);
+  useEffect(() => {
+    // Only show tables of the selected schema
+    const tables = vmd.getSchema(schemaName)?.tables || [];
+    setTableList(tables);
+  }, [schemaName]);
 
-    const resetForm = ()=>{
-        setViewName('');
-        setViewType(1);
-        setTableName('');
-        const form = document.getElementById('AdditionalViewModalForm');
-        form && form.reset();
-    }
-    const handleClose = () => {
-        setShow(false);
-        resetForm();
-    };
+  const resetForm = () => {
+    setViewName("");
+    setViewType(1);
+    setTableName("");
+    const form = document.getElementById("AdditionalViewModalForm");
+    form && form.reset();
+  };
+  const handleClose = () => {
+    setShow(false);
+    resetForm();
+  };
 
-    const handleSave = (e): void =>{
-        e.preventDefault();
-        insertNewView(schemaName, tableName, viewName, viewType, customCode);
-        handleClose();
-        updateList();
-    }
+  const handleSave = (e): void => {
+    e.preventDefault();
+    insertNewView(schemaName, tableName, viewName, viewType, customCode);
+    handleClose();
+    updateList();
+  };
 
-    const handleFileChange = (e):void =>{
-        const [file] = e.target.files;
-        const reader = new FileReader();
-        reader.addEventListener(
-            "load",
-            () => {
-                const text = reader.result;
-                setCustomCode(text);
-            },
-            false
-          );
-        if (file) {
-            reader.readAsText(file);
-        }
-    }
-    return (
-        <>
-        <Modal show={show} onHide={handleClose} id='AdditionalViewModal'>
-            <Modal.Header closeButton>
-              <Modal.Title>Add a new view</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSave} id='AdditionalViewModalForm'>
-                    <Form.Group className="mb-3" controlId="viewName">
-                        <Form.Label>view name</Form.Label>
-                        <Form.Control name='viewName' type="text" onChange={(e)=>{setViewName(e.target.value)}} placeholder="Enter a view name"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="viewType">
-                        <Form.Label>view type</Form.Label>
-                        <Form.Select name='viewType' onChange={(e)=>setViewType(parseInt(e.target.value))}>
-                            <option value={ViewTypeEnum.Calendar}>Calendar</option>
-                            <option value={ViewTypeEnum.Timeline}>Timeline</option>
-                            <option value={ViewTypeEnum.TreeView}>Tree View</option>
-                            <option value={ViewTypeEnum.Custom}>Custom</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="viewTable">
-                        <Form.Label>tables</Form.Label>
-                        <Form.Select onChange={(e)=>setTableName(e.target.value)}>
-                            {tableList && tableList.map((table)=>
-                                <option key={table.table_name} value={table.table_name}>{table.table_name}</option>
-                            )}
-                        </Form.Select>
-                    </Form.Group>
-                    {(viewType === ViewTypeEnum.Custom) &&
-                    (
-                    <Form.Group className="mb-3" controlId="viewCustomCode">
-                        <Form.Label>view custom code</Form.Label>
-                        <Form.Control type="file" accept='.ts, .tsx, ,js, .jsx, .txt' onChange={(e)=>handleFileChange(e)} />
-                    </Form.Group>
-                    )}
-                </Form>
-
-            </Modal.Body>
-
-            <Modal.Footer>
-                <Button className="buttonStyle" variant="contained" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="contained" onClick={handleSave}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
-          </Modal>
-
-        </>
+  const handleFileChange = (e): void => {
+    const [file] = e.target.files;
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      () => {
+        const text = reader.result;
+        setCustomCode(text);
+      },
+      false,
     );
-}
+    if (file) {
+      reader.readAsText(file);
+    }
+  };
+  return (
+    <>
+      <Modal show={show} onHide={handleClose} id="AdditionalViewModal">
+        <Modal.Header closeButton>
+          <Modal.Title>Add a new view</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSave} id="AdditionalViewModalForm">
+            <Form.Group className="mb-3" controlId="viewName">
+              <Form.Label>view name</Form.Label>
+              <Form.Control
+                name="viewName"
+                type="text"
+                onChange={(e) => {
+                  setViewName(e.target.value);
+                }}
+                placeholder="Enter a view name"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="viewType">
+              <Form.Label>view type</Form.Label>
+              <Form.Select
+                name="viewType"
+                onChange={(e) => setViewType(parseInt(e.target.value))}
+              >
+                <option value={ViewTypeEnum.Calendar}>Calendar</option>
+                <option value={ViewTypeEnum.Timeline}>Timeline</option>
+                <option value={ViewTypeEnum.TreeView}>Tree View</option>
+                <option value={ViewTypeEnum.Custom}>Custom</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="viewTable">
+              <Form.Label>tables</Form.Label>
+              <Form.Select onChange={(e) => setTableName(e.target.value)}>
+                {tableList &&
+                  tableList.map((table) => (
+                    <option key={table.table_name} value={table.table_name}>
+                      {table.table_name}
+                    </option>
+                  ))}
+              </Form.Select>
+            </Form.Group>
+            {viewType === ViewTypeEnum.Custom && (
+              <Form.Group className="mb-3" controlId="viewCustomCode">
+                <Form.Label>view custom code</Form.Label>
+                <Form.Control
+                  type="file"
+                  accept=".ts, .tsx, ,js, .jsx, .txt"
+                  onChange={(e) => handleFileChange(e)}
+                />
+              </Form.Group>
+            )}
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            className="buttonStyle"
+            variant="contained"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+          <Button variant="contained" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default AdditionalViewModal;
--- UInnovateApp/src/pages/ObjectMenu.tsx
+++ UInnovateApp/src/pages/ObjectMenu.tsx
@@ -12,42 +12,127 @@ import { LOGIN_BYPASS } from "../redux/AuthSlice";
 import { useNavigate, useParams } from "react-router-dom";
 import { updateSelectedSchema } from "../redux/SchemaSlice";
 import AdditionalViewNavBar from "../components/AdditionalViewNavBar";
+import { ViewTypeEnum } from "../enums/ViewTypeEnum";
+
+interface viewSelection {
+  schema: string;
+  tableName: string;
+  selectedView: ViewTypeEnum;
+}
 
 export function ObjectMenu() {
   const selectedSchema: string = useSelector(
-    (state: RootState) => state.schema.value
+    (state: RootState) => state.schema.value,
   );
-  const navigate = useNavigate()
-  const handleTableSelect = (tableName: Table) => {
-    setActiveTable(tableName);
-    const schema = vmd.getTableSchema(tableName.table_name)
+  const navigate = useNavigate();
+  const handleTableSelect = (table: Table) => {
+    setActiveTable(table);
+    const schema = vmd.getTableSchema(table.table_name);
     // Navigate to the TableListView route with the selected table name
-    navigate(`/${schema?.schema_name.toLowerCase()}/${tableName.table_name.toLowerCase()}`);
+
+    setViewForTable(schema?.schema_name, table.table_name);
+
+    navigate(
+      `/${schema?.schema_name.toLowerCase()}/${table.table_name.toLowerCase()}`,
+    );
+  };
+
+  const setViewForTable = (
+    p_schema: string | undefined,
+    p_tableName: string,
+  ) => {
+    if (p_schema && p_tableName) {
+      //retrieve view selection from list
+      let selection = selectedViewList.filter((v) => {
+        if (v.schema == p_schema && v.tableName == p_tableName) return true;
+        return false;
+      });
+
+      let viewType = ViewTypeEnum.Default;
+      //add default if it dosen't exist
+      if (selection.length == 0) {
+        const view: viewSelection = {
+          schema: p_schema,
+          tableName: p_tableName,
+          selectedView: viewType,
+        };
+
+        setSelectedViewList([...selectedViewList, view]);
+      } else {
+        //get existing value
+        viewType = selection[0].selectedView;
+      }
+      console.log(`view type: ${viewType}`);
+      setViewType(viewType);
+    }
   };
   const dispatch = useDispatch();
-  const { tableName } = useParams()
-  const {schema} = useParams()
+  const { tableName } = useParams();
+  const { schema } = useParams();
+
+  const [selectedViewList, setSelectedViewList] = useState<
+    Array<viewSelection>
+  >([]);
+  const [viewType, setViewType] = useState<ViewTypeEnum>(ViewTypeEnum.Default);
+  const selectViewsStorageKey = "objectMenu_selectedViews";
 
   const [activeTable, setActiveTable] = useState<Table | null>(null);
-  
+
   // Get the visible tables from the VMD for the selected schema
   const tables = vmd.getVisibleTables(schema ?? selectedSchema);
-  
+
+  const handleViewTypeSelection = (
+    p_schema: string,
+    p_tableName: string,
+    p_viewType: ViewTypeEnum,
+  ) => {
+    //get list without schema table
+    const filteredList = selectedViewList.filter((v) => {
+      if (v.schema == p_schema && v.tableName == p_tableName) return false;
+      return true;
+    });
+    const newEntry = {
+      schema: p_schema,
+      tableName: p_tableName,
+      selectedView: p_viewType,
+    };
+    setSelectedViewList([...filteredList, newEntry]);
+    setViewType(p_viewType);
+  };
+
   useEffect(() => {
     dispatch(updateSelectedSchema(schema ?? ""));
     setActiveTable(null);
-  }, [schema]);
-  const {user, schema_access} = useSelector((state: RootState) => state.auth);
 
+    const storedState = localStorage.getItem(selectViewsStorageKey);
+    if (storedState) {
+      setSelectedViewList(JSON.parse(storedState));
+    }
+
+    return () => {
+      // Save your state when navigating away
+      localStorage.setItem(
+        selectViewsStorageKey,
+        JSON.stringify(selectedViewList),
+      );
+    };
+  }, [schema]);
+  const { user, schema_access } = useSelector((state: RootState) => state.auth);
 
   return (
     <>
       <NavBar />
-      {(user === null && !LOGIN_BYPASS) || (user !== null && schema && !schema_access.includes(schema)) ?
+      {(user === null && !LOGIN_BYPASS) ||
+      (user !== null && schema && !schema_access.includes(schema)) ? (
         <UnauthorizedScreen />
-        :
+      ) : (
         <>
-          <AdditionalViewNavBar selectedSchema={selectedSchema} selectedTable={activeTable} />
+          <AdditionalViewNavBar
+            selectedSchema={selectedSchema}
+            selectedTable={activeTable}
+            selectedViewType={viewType}
+            selectViewHandler={handleViewTypeSelection}
+          />
           <div className="page-container">
             <h1 className="title">Tables</h1>
             <Tab.Container activeKey={tableName}>
@@ -70,24 +155,51 @@ export function ObjectMenu() {
                 </Col>
                 <Col sm={9}>
                   <Tab.Content>
-                    {tables?.map((table: Table) => (
-                      <Tab.Pane key={table.table_name} eventKey={table.table_name}>
-                        {tableName === table.table_name ? (
-                          table.table_display_type === "list" ? (
-                            <TableListView table={table}></TableListView>
-                          ) : table.table_display_type === "enum" ? (
-                            <TableEnumView table={table}></TableEnumView>
-                          ) : null
-                        ) : null}
-                      </Tab.Pane>
-                    ))}
+                    {viewType == ViewTypeEnum.Default && (
+                      <>
+                        {tables?.map((table: Table) => (
+                          <Tab.Pane
+                            key={table.table_name}
+                            eventKey={table.table_name}
+                          >
+                            {tableName === table.table_name ? (
+                              table.table_display_type === "list" ? (
+                                <TableListView table={table}></TableListView>
+                              ) : table.table_display_type === "enum" ? (
+                                <TableEnumView table={table}></TableEnumView>
+                              ) : null
+                            ) : null}
+                          </Tab.Pane>
+                        ))}
+                      </>
+                    )}
+                    {viewType == ViewTypeEnum.Calendar && (
+                      <>
+                        <span>Calendar view</span>
+                      </>
+                    )}
+                    {viewType == ViewTypeEnum.Timeline && (
+                      <>
+                        <span>Timeline view</span>
+                      </>
+                    )}
+                    {viewType == ViewTypeEnum.TreeView && (
+                      <>
+                        <span>TreeView view</span>
+                      </>
+                    )}
+                    {viewType == ViewTypeEnum.Custom && (
+                      <>
+                        <span>Custom view</span>
+                      </>
+                    )}
                   </Tab.Content>
                 </Col>
               </Row>
             </Tab.Container>
           </div>
         </>
-      }
+      )}
     </>
   );
 }
