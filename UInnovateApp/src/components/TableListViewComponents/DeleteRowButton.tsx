c03851a8efd16bce613b3259eb2326d62478e715
MaximeJoanisse <maxjoanisse101@gmail.com>
2024-03-16 17:01:42 -0400

    Added Tests for #309
    Test have been added for the button function

--- UInnovateApp/src/components/TableListViewComponents/DeleteRowButton.tsx
+++ UInnovateApp/src/components/TableListViewComponents/DeleteRowButton.tsx
@@ -13,19 +13,32 @@ interface DeleteRowProps {
 
 const DeleteRowButton: React.FC<DeleteRowProps> =
   ({ getRows, table, row }: DeleteRowProps): ReactNode => {
-
+    //for the error display message
     const dispatch = useDispatch();
-
     const schema = vmd.getTableSchema(table.table_name);
     if (!schema) {
       console.error("Schema not found");
       return;
     }
 
+    //finding the dependent tables if there are any 
+    const columnWithReference = table.columns.find(column => column.referenced_table != null);
+    let referencedTables: string = "";
+    if (columnWithReference) {
+      referencedTables = columnWithReference.referenced_table as string;
+    }
+
+
+
+
+    //deleting function
     const DeleteRow = async () => {
+      console.log(table);
+
       let column_name: string | undefined;
       let column_value: string | undefined;
 
+      // Finding the primary key
       table.columns.forEach((column) => {
         if (column.is_editable === false) {
           // Check if the row contains the key
@@ -44,37 +57,34 @@ const DeleteRowButton: React.FC<DeleteRowProps> =
 
         try {
           await data_accessor_delete.deleteRow();
-          console.log("Row deletion successful");
         } catch (error) {
           if (error) {
-            dispatch(displayError("The row could not be deleted due to dependencies."));
+            dispatch(displayError(`The row could not be deleted due to dependencies with ${referencedTables}.`));
           }
+          
         }
         getRows();
 
       }
       else {
-        console.log("No Primary Key Found");
+        //if the table has no primary keys
         const rowKeys: string[] = Object.keys(row.row || {});
         const rowValues: string[] = Object.values(row.row || {}).map(String);
-        console.log(rowKeys)
-        console.log(rowValues)
         const data_accessor_delete: DataAccessor = vmd.getRemoveRowAccessor(
           schema.schema_name,
           table.table_name,
           rowKeys,
           rowValues
         );
-          
-          try {
-            await data_accessor_delete.deleteRow();
-            console.log("Row deletion successful");
-          } catch (error) {
-            if (error) {
-              dispatch(displayError("The row could not be deleted due to dependencies."));
-            }
+
+        try {
+          await data_accessor_delete.deleteRow();
+        } catch (error) {
+          if (error) {
+            dispatch(displayError(`The row could not be deleted due to dependencies with ${referencedTables}.`));
           }
-          getRows();
+        }
+        getRows();
 
       }
 
