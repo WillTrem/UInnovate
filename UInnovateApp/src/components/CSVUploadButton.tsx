6c65c30e6750b6a9b8a74e1ac2cf23185bdd4bbf
William Tremblay <wt22@videotron.ca>
2024-03-28 19:53:22 -0400

    #225 Enabled repeatedly uploading files

--- UInnovateApp/src/components/CSVUploadButton.tsx
+++ UInnovateApp/src/components/CSVUploadButton.tsx
@@ -15,44 +15,46 @@ interface CSVUploadButtonProps {
 	getRows: () => Promise<void>;
 }
 
-export const CSVUploadButton: React.FC<CSVUploadButtonProps> = ({table, getRows}) => {
+export const CSVUploadButton: React.FC<CSVUploadButtonProps> = ({ table, getRows }) => {
 	const dispatch = useDispatch();
-	const loggedInUser  = useSelector((state: RootState) => state.auth.user);
-	function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>){
+	const loggedInUser = useSelector((state: RootState) => state.auth.user);
+	function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
 		const file = event?.target?.files?.[0];
-		if(file){
-			if(file.type !== 'text/csv'){
+		if (file) {
+			if (file.type !== 'text/csv') {
 				dispatch(displayError(`ERROR: '${file.name}' is not in CSV format.`))
-				return;
 			}
-			parse(file, {header: true, skipEmptyLines: true, complete: async (result) => {
-				console.log(result);
-				try{
-					validateCSV(result, table);
-					await loadCSVToDB(result, table);
-					dispatch(displayNotification(`Succesfully loaded ${file.name} in table ${table.table_name}`));
-					const schema = VMD.getTableSchema(table.table_name);
-					Logger.logUserAction( 
-						loggedInUser || "",
-						"Loaded CSV data",
-						`Loaded CSV data from ${file.name} (${result.data.length} rows)`,
-						schema?.schema_name || "",
-						table.table_name
-					  );
-					  getRows();
-				}
-				catch(error: any){
-					if(error.response){
-						dispatch(displayError(`ERROR: ${error.response.data.message}`))
+			else {
+				parse(file, {
+					header: true, skipEmptyLines: true, complete: async (result) => {
+						console.log(result);
+						try {
+							validateCSV(result, table);
+							await loadCSVToDB(result, table);
+							dispatch(displayNotification(`Succesfully loaded ${file.name} in table ${table.table_name}`));
+							const schema = VMD.getTableSchema(table.table_name);
+							Logger.logUserAction(
+								loggedInUser || "",
+								"Loaded CSV data",
+								`Loaded CSV data from ${file.name} (${result.data.length} rows)`,
+								schema?.schema_name || "",
+								table.table_name
+							);
+							getRows();
+						}
+						catch (error: any) {
+							if (error.response) {
+								dispatch(displayError(`ERROR: ${error.response.data.message}`))
+							}
+							else if (error instanceof Error) {
+								dispatch(displayError(`ERROR: ${error.message}`))
+							}
+							// Reset the value of the file input
+						}
 					}
-					else if(error instanceof Error){
-						dispatch(displayError(`ERROR: ${error.message}`))
-					}
-					// Reset the value of the file input
-					event.target.value = '';
-				}
-			}}) 
-			
+				})
+			}
+			event.target.value = '';
 		}
 	}
 	return <Button
@@ -60,7 +62,7 @@ export const CSVUploadButton: React.FC<CSVUploadButtonProps> = ({table, getRows}
 		role={undefined}
 		variant="contained"
 		tabIndex={-1}
-		endIcon={<UploadFile fontSize="small"/>}
+		endIcon={<UploadFile fontSize="small" />}
 		style={{
 			backgroundColor: "#404040",
 			width: "fit-content",
